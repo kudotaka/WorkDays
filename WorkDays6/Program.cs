13b340539129505184e15458e8b3eb867ab1edcb
kudotaka <kudotaka@aurora.ocn.ne.jp>
2024-11-13 13:31:36 +0000

    Change commit

---
+++ WorkDays6/Program.cs
@@ -0,0 +1,247 @@
+﻿﻿using System.Text;
+using ClosedXML.Excel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Utf8StringInterpolation;
+using ZLogger;
+using ZLogger.Providers;
+
+//==
+var builder = ConsoleApp.CreateBuilder(args);
+builder.ConfigureServices((ctx,services) =>
+{
+    // Register appconfig.json to IOption<MyConfig>
+    services.Configure<MyConfig>(ctx.Configuration);
+
+    // Using Cysharp/ZLogger for logging to file
+    services.AddLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.SetMinimumLevel(LogLevel.Trace);
+        var jstTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+        var utcTimeZoneInfo = TimeZoneInfo.Utc;
+        logging.AddZLoggerConsole(options =>
+        {
+            options.UsePlainTextFormatter(formatter => 
+            {
+                formatter.SetPrefixFormatter($"{0:yyyy-MM-dd'T'HH:mm:sszzz}|{1:short}|", (in MessageTemplate template, in LogInfo info) => template.Format(TimeZoneInfo.ConvertTime(info.Timestamp.Utc, jstTimeZoneInfo), info.LogLevel));
+                formatter.SetExceptionFormatter((writer, ex) => Utf8String.Format(writer, $"{ex.Message}"));
+            });
+        });
+        logging.AddZLoggerRollingFile(options =>
+        {
+            options.UsePlainTextFormatter(formatter => 
+            {
+                formatter.SetPrefixFormatter($"{0:yyyy-MM-dd'T'HH:mm:sszzz}|{1:short}|", (in MessageTemplate template, in LogInfo info) => template.Format(TimeZoneInfo.ConvertTime(info.Timestamp.Utc, jstTimeZoneInfo), info.LogLevel));
+                formatter.SetExceptionFormatter((writer, ex) => Utf8String.Format(writer, $"{ex.Message}"));
+            });
+
+            // File name determined by parameters to be rotated
+            options.FilePathSelector = (timestamp, sequenceNumber) => $"logs/{timestamp.ToLocalTime():yyyy-MM-dd}_{sequenceNumber:00}.log";
+            
+            // The period of time for which you want to rotate files at time intervals.
+            options.RollingInterval = RollingInterval.Day;
+            
+            // Limit of size if you want to rotate by file size. (KB)
+            options.RollingSizeKB = 1024;        
+        });
+    });
+});
+
+var app = builder.Build();
+app.AddCommands<WorkDaysApp>();
+app.Run();
+
+
+public class WorkDaysApp : ConsoleAppBase
+{
+    bool isAllPass = true;
+
+    readonly ILogger<WorkDaysApp> logger;
+    readonly IOptions<MyConfig> config;
+
+    List<MyWorkDay> listMyWorkDay = new List<MyWorkDay>();
+
+    public WorkDaysApp(ILogger<WorkDaysApp> logger,IOptions<MyConfig> config)
+    {
+        this.logger = logger;
+        this.config = config;
+    }
+
+//    [Command("")]
+    public void Days(string firstexcel, string secondexcel)
+    {
+//== start
+        logger.ZLogInformation($"==== tool {getMyFileVersion()} ====");
+        if (!File.Exists(firstexcel))
+        {
+            logger.ZLogError($"[NG] first excel file is missing.");
+            return;
+        }
+        if (!File.Exists(secondexcel))
+        {
+            logger.ZLogError($"[NG] second excel file is missing.");
+            return;
+        }
+
+        string firstExcelSheetName = config.Value.FirstExcelSheetName;
+        string secondExcelSheetName = config.Value.SecondExcelSheetName;
+        int firstDataRow = config.Value.FirstDataRow;
+        int siteNumberColumn = config.Value.SiteNumberColumn;
+        int siteNameColumn = config.Value.SiteNameColumn;
+        int workDayCountColumn = config.Value.WorkDayCountColumn;
+        int workDaysColumn = config.Value.WorkDaysColumn;
+
+        FileStream fsFirstExcel = new FileStream(firstexcel, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using XLWorkbook xlWorkbookFristExcel = new XLWorkbook(fsFirstExcel);
+        IXLWorksheets sheetsFristExcel = xlWorkbookFristExcel.Worksheets;
+        foreach (IXLWorksheet? sheet in sheetsFristExcel)
+        {
+            if (firstExcelSheetName.Equals(sheet.Name))
+            {
+                int lastUsedRowNumber = sheet.LastRowUsed() == null ? 0 : sheet.LastRowUsed().RowNumber();
+                logger.ZLogInformation($"シート名:{sheet.Name}, 最後の行:{lastUsedRowNumber}");
+
+                for (int r = firstDataRow; r < lastUsedRowNumber + 1; r++)
+                {
+                    IXLCell cellWorkDayCount = sheet.Cell(r, workDayCountColumn);
+                    int workCount = -1;
+                    switch (cellWorkDayCount.DataType)
+                    {
+                        case XLDataType.Number:
+                            workCount = cellWorkDayCount.GetValue<int>();
+                            break;
+                        case XLDataType.Text:
+                            break;
+                        default:
+                            logger.ZLogError($"workCount is NOT type ( Number | Text ) at sheet:{sheet.Name} row:{r}");
+                            continue;
+                    }
+                    IXLCell cellWorkDaysColumn = sheet.Cell(r, workDaysColumn);
+
+                    string workDays = cellWorkDaysColumn.GetValue<string>();
+                    logger.ZLogInformation($"工事日数:{workCount}, 工事日:{workDays}");
+                    MyWorkDay wd = new MyWorkDay();
+                    wd.workDayCount = workCount;
+                    List<DateTime> listDateTime = new List<DateTime>();
+                    foreach (var day in workDays.Split("|"))
+                    {
+                        DateTime dt = DateTime.Parse(day);
+                        listDateTime.Add(dt);
+                    }
+                    wd.workDays = listDateTime;
+                    wd.siteNumber = sheet.Cell(r, siteNumberColumn).Value.ToString();
+                    wd.siteName = sheet.Cell(r, siteNameColumn).Value.ToString();
+
+                    listMyWorkDay.Add(wd);
+                }
+            }
+            else
+            {
+                logger.ZLogTrace($"Miss {sheet.Name}");
+            }
+        }
+
+
+
+//== print
+        printMyWorkDays();
+
+//== check
+        checkWorkDayCount();
+
+//== finish
+        if (isAllPass)
+        {
+            logger.ZLogInformation($"== [Congratulations!] すべての確認項目をパスしました ==");
+        }
+        logger.ZLogInformation($"==== tool finish ====");
+    }
+
+
+    private void checkWorkDayCount()
+    {
+        logger.ZLogInformation($"== start 工事日数と工事日の日数一致の確認 ==");
+        bool isError = false;
+        foreach (var workDay in listMyWorkDay)
+        {
+            if (workDay.workDayCount == workDay.workDays.Count)
+            {
+                logger.ZLogTrace($"[checkWorkDayCount] 一致");
+            }
+            else
+            {
+                isError = true;
+                logger.ZLogError($"不一致エラー siteNumber:{workDay.siteNumber},siteName:{workDay.siteName}");
+            }
+        }
+        if (isError)
+        {
+            isAllPass = false;
+            logger.ZLogInformation($"[NG] 工事日数と工事日の日数の不一致が発見されました");
+        }
+        else
+        {
+            logger.ZLogInformation($"[OK] 工事日数と工事日の日数の不一致はありませんでした");
+        }
+        logger.ZLogInformation($"== end 工事日数と工事日の日数一致の確認 ==");
+    }
+
+    private void printMyWorkDays()
+    {
+        logger.ZLogTrace($"== start print ==");
+        foreach (var workDay in listMyWorkDay)
+        {
+//            logger.ZLogTrace($"workDayCount:{workDay.workDayCount},workDays:{string.Join(";",workDay.workDays)}");
+            logger.ZLogTrace($"siteNumber:{workDay.siteNumber},siteName:{workDay.siteName},workDayCount:{workDay.workDayCount},workDays:{convertDateTimeToDate(workDay.workDays)}");
+        }
+        logger.ZLogTrace($"== end print ==");
+    }
+
+    private string convertDateTimeToDate(List<DateTime> listDateTime)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < listDateTime.Count; i++)
+        {
+            sb.Append(listDateTime[i].ToString("yyyy/MM/dd"));
+            if (i < listDateTime.Count - 1)
+            {
+                sb.Append(" & ");
+            }
+        }
+        return sb.ToString();
+    }
+
+    private string getTime()
+    {
+        var jstTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+        return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, jstTimeZoneInfo).ToString("yyyy-MM-dd'T'HH:mm:sszzz");
+    }
+
+    private string getMyFileVersion()
+    {
+        System.Diagnostics.FileVersionInfo ver = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        return ver.InternalName + "(" + ver.FileVersion + ")";
+    }
+}
+
+//==
+public class MyConfig
+{
+    public int FirstDataRow {get; set;} = -1;
+    public int SiteNumberColumn {get; set;} = -1;
+    public int SiteNameColumn {get; set;} = -1;
+    public int WorkDayCountColumn {get; set;} = -1;
+    public int WorkDaysColumn {get; set;} = -1;
+    public string FirstExcelSheetName {get; set;} = "";
+    public string SecondExcelSheetName {get; set;} = "";
+}
+
+public class MyWorkDay
+{
+    public string siteNumber = "";
+    public string siteName = "";
+    public int workDayCount = -1;
+    public List<DateTime> workDays = new List<DateTime>();
+}
