f2a6a5f2cb964cd052776aab23aa4b74fa1200e2
kudotaka <kudotaka@aurora.ocn.ne.jp>
2024-11-16 02:21:21 +0000

    Change commit

--- WorkDays6/Program.cs
+++ WorkDays6/Program.cs
@@ -70,7 +70,7 @@ public class WorkDaysApp : ConsoleAppBase
     }
 
 //    [Command("")]
-    public void Days(string firstexcel, string secondexcel)
+    public void Days(string firstexcel, string secondexcel, string printday)
     {
 //== start
         logger.ZLogInformation($"==== tool {getMyFileVersion()} ====");
@@ -88,8 +88,10 @@ public class WorkDaysApp : ConsoleAppBase
         string firstExcelSheetName = config.Value.FirstExcelSheetName;
         string secondExcelSheetName = config.Value.SecondExcelSheetName;
         int firstDataRow = config.Value.FirstDataRow;
+        int siteKeyColumn = config.Value.SiteKeyColumn;
         int siteNumberColumn = config.Value.SiteNumberColumn;
         int siteNameColumn = config.Value.SiteNameColumn;
+        int statusColumn = config.Value.StatusColumn;
         int workDayCountColumn = config.Value.WorkDayCountColumn;
         int workDaysColumn = config.Value.WorkDaysColumn;
 
@@ -124,6 +126,10 @@ public class WorkDaysApp : ConsoleAppBase
                     logger.ZLogTrace($"工事日数:{workCount}, 工事日:{workDays}");
                     MyWorkDay wd = new MyWorkDay();
                     wd.workDayCount = workCount;
+                    wd.siteNumber = sheet.Cell(r, siteNumberColumn).Value.ToString();
+                    wd.siteName = sheet.Cell(r, siteNameColumn).Value.ToString();
+                    wd.siteKey = sheet.Cell(r, siteKeyColumn).Value.ToString();
+                    wd.status = sheet.Cell(r, statusColumn).Value.ToString();
                     List<DateTime> listDateTime = new List<DateTime>();
                     foreach (var day in workDays.Split("|"))
                     {
@@ -132,10 +138,12 @@ public class WorkDaysApp : ConsoleAppBase
                             DateTime dt = DateTime.Parse(day);
                             listDateTime.Add(dt);
                         }
-                        catch (FormatException fe)
+                        catch (FormatException)
                         {
                             isAllPass = false;
-                            logger.ZLogError($"DateTime.Parse() exception:{fe.ToString()}");
+                            DateTime errDt = new DateTime(1900,1,1);
+                            listDateTime.Add(errDt);
+                            logger.ZLogTrace($"エラー 日付に変換できませんでした:{day},拠点名:{wd.siteName}");
                         }
                         catch (System.Exception)
                         {
@@ -144,9 +152,6 @@ public class WorkDaysApp : ConsoleAppBase
                         }
                     }
                     wd.workDays = listDateTime;
-                    wd.siteNumber = sheet.Cell(r, siteNumberColumn).Value.ToString();
-                    wd.siteName = sheet.Cell(r, siteNameColumn).Value.ToString();
-
                     listMyWorkDay.Add(wd);
                 }
             }
@@ -167,6 +172,9 @@ public class WorkDaysApp : ConsoleAppBase
 //== check
         checkWorkDayAtDayOfWeek();
 
+//== print day
+        printToday(printday);
+
 //== finish
         if (isAllPass)
         {
@@ -175,29 +183,87 @@ public class WorkDaysApp : ConsoleAppBase
         logger.ZLogInformation($"==== tool finish ====");
     }
 
+    private void printToday(string printday)
+    {
+        logger.ZLogInformation($"== start 工事日の拠点 ==");
+        bool isDateError = false;
+        StringBuilder sb = new StringBuilder();
+        string checkStatusAtWork = config.Value.CheckStatusAtWork;
+        try
+        {
+            DateTime day = DateTime.Parse(printday);
+            sb.AppendLine($"");
+            sb.AppendLine($"{convertDateTimeToDateAndDayofweek(day)} の拠点は以下です");
+            foreach (var workDay in listMyWorkDay)
+            {
+                if (checkStatusAtWork.Equals(workDay.status))
+                {
+                    if (workDay.workDays.Contains(new DateTime(1900,1,1)))
+                    {
+                        isDateError = true;
+                        logger.ZLogError($"日付エラー key:{workDay.siteKey},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName},工事日:{convertDateTimeToDate(workDay.workDays)}");
+                        continue;
+                    }
+                    int index = workDay.workDays.IndexOf(day)+1;
+                    if (index > 0)
+                    {
+                        sb.AppendLine($"{workDay.siteNumber}_{workDay.siteName} ({index})");
+                    }
+                }
+            }
+            logger.ZLogInformation($"{sb.ToString()}");
+        }
+        catch (FormatException)
+        {
+            isAllPass = false;
+            logger.LogError($"[NG] エラー 日付に変換できませんでした:{printday}");
+        }
+        catch (System.Exception)
+        {
+            isAllPass = false;
+            throw;
+        }
+        logger.ZLogInformation($"== end 工事日の拠点 ==");
+    }
 
     private void checkWorkDayCount()
     {
         logger.ZLogInformation($"== start 工事日数と工事日の日数一致の確認 ==");
+        bool isDateError = false;
         bool isError = false;
+        string checkStatusAtWork = config.Value.CheckStatusAtWork;
         foreach (var workDay in listMyWorkDay)
         {
-            if (workDay.workDayCount == workDay.workDays.Count)
+            if (checkStatusAtWork.Equals(workDay.status))
             {
-                logger.ZLogTrace($"[checkWorkDayCount] 一致");
+                if (workDay.workDays.Contains(new DateTime(1900,1,1)))
+                {
+                    isDateError = true;
+                    logger.ZLogError($"日付エラー key:{workDay.siteKey},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName},工事日:{convertDateTimeToDate(workDay.workDays)}");
+                    continue;
+                }
+                if (workDay.workDayCount == workDay.workDays.Count)
+                {
+                    logger.ZLogTrace($"[checkWorkDayCount] 一致");
+                }
+                else
+                {
+                    isError = true;
+                    logger.ZLogError($"不一致エラー 拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName},工事日数:{workDay.workDayCount},工事日:{convertDateTimeToDate(workDay.workDays)}");
+                }
             }
-            else
-            {
-                isError = true;
-                logger.ZLogError($"不一致エラー 拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName},工事日数:{workDay.workDayCount},工事日:{convertDateTimeToDate(workDay.workDays)}");
-            }
+        }
+        if (isDateError)
+        {
+            isAllPass = false;
+            logger.ZLogError($"[ERROR] 日付にエラーが発見されました");
         }
         if (isError)
         {
             isAllPass = false;
             logger.ZLogInformation($"[NG] 工事日数と工事日の日数の不一致が発見されました");
         }
-        else
+        if (!isDateError && !isError)
         {
             logger.ZLogInformation($"[OK] 工事日数と工事日の日数の不一致はありませんでした");
         }
@@ -207,11 +273,12 @@ public class WorkDaysApp : ConsoleAppBase
     private void checkWorkDayAtDayOfWeek()
     {
         logger.ZLogInformation($"== start 工事日と曜日の確認 ==");
-        bool isError = false;
+        bool isDateError = false;
+        bool isWarning = false;
         Dictionary<string,DateTime> dicHolidays = new Dictionary<string, DateTime>();
+        string checkStatusAtWork = config.Value.CheckStatusAtWork;
         string publicHolidaysInJapan = config.Value.PublicHolidaysInJapan;
         string bussinessHolidays = config.Value.BussinessHolidays;
-logger.ZLogInformation($"[checkWorkDayAtDayOfWeek] {bussinessHolidays}");
         foreach (var holiday in publicHolidaysInJapan.Split('|'))
         {
             dicHolidays.Add(holiday, DateTime.Parse(holiday));
@@ -220,46 +287,61 @@ logger.ZLogInformation($"[checkWorkDayAtDayOfWeek] {bussinessHolidays}");
         {
             dicHolidays.Add(holiday, DateTime.Parse(holiday));
         }
-logger.ZLogInformation($"[checkWorkDayAtDayOfWeek] {convertDateTimeToDate(dicHolidays.Values.ToList<DateTime>())}");
+        logger.ZLogTrace($"[checkWorkDayAtDayOfWeek] {bussinessHolidays}");
+        logger.ZLogTrace($"[checkWorkDayAtDayOfWeek] {convertDateTimeToDate(dicHolidays.Values.ToList<DateTime>())}");
         foreach (var workDay in listMyWorkDay)
         {
-            foreach (var day in workDay.workDays)
+            if (checkStatusAtWork.Equals(workDay.status))
             {
-                if (dicHolidays.ContainsKey(day.ToString("yyyy/MM/dd")))
+                if (workDay.workDays.Contains(new DateTime(1900,1,1)))
                 {
-                    isError = true;
-                    logger.ZLogWarning($"要注意！ 休日:{day.ToString("yyyy/MM/dd")},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName}");
+                    isDateError = true;
+                    logger.ZLogError($"日付エラー key:{workDay.siteKey},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName},工事日:{convertDateTimeToDate(workDay.workDays)}");
+                    continue;
                 }
-                else
+                foreach (var day in workDay.workDays)
                 {
-                    switch (day.DayOfWeek)
+                    if (dicHolidays.ContainsKey(day.ToString("yyyy/MM/dd")))
                     {
-                        case DayOfWeek.Sunday:
-                            isError = true;
-                            logger.ZLogWarning($"要注意！ 日曜:{day.ToString("yyyy/MM/dd")},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName}");
-                            break;
-                        case DayOfWeek.Saturday:
-                            isError = true;
-                            logger.ZLogWarning($"要注意！ 土曜:{day.ToString("yyyy/MM/dd")},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName}");
-                            break;
-                        default:
-                            logger.ZLogTrace($"[checkWorkDayAtDayOfWeek] 平日:{day.ToString("yyyy/MM/dd")}");
-                            break;
+                        isWarning = true;
+                        logger.ZLogWarning($"要注意！ 休日:{day.ToString("yyyy/MM/dd")},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName}");
+                    }
+                    else
+                    {
+                        switch (day.DayOfWeek)
+                        {
+                            case DayOfWeek.Sunday:
+                                isWarning = true;
+                                logger.ZLogWarning($"要注意！ 日曜:{day.ToString("yyyy/MM/dd")},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName}");
+                                break;
+                            case DayOfWeek.Saturday:
+                                isWarning = true;
+                                logger.ZLogWarning($"要注意！ 土曜:{day.ToString("yyyy/MM/dd")},拠点番号:{workDay.siteNumber},拠点名:{workDay.siteName}");
+                                break;
+                            default:
+//                                logger.ZLogTrace($"[checkWorkDayAtDayOfWeek] 平日:{day.ToString("yyyy/MM/dd")}");
+                                break;
+                        }
                     }
                 }
             }
+            else
+            {
+                logger.ZLogTrace($"除外しました key:{workDay.siteKey},status:{workDay.status}");
+            }
         }
 
-
-
-
-
-        if (isError)
+        if (isDateError)
+        {
+            isAllPass = false;
+            logger.ZLogError($"[ERROR] 日付にエラーが発見されました");
+        }
+        if (isWarning)
         {
             isAllPass = false;
             logger.ZLogInformation($"[WARNING] 工事日と曜日に土日祝が発見されました");
         }
-        else
+        if (!isDateError && !isWarning)
         {
             logger.ZLogInformation($"[OK] 工事日と曜日に土日祝は含まれていませんでした");
         }
@@ -272,7 +354,7 @@ logger.ZLogInformation($"[checkWorkDayAtDayOfWeek] {convertDateTimeToDate(dicHol
         foreach (var workDay in listMyWorkDay)
         {
 //            logger.ZLogTrace($"workDayCount:{workDay.workDayCount},workDays:{string.Join(";",workDay.workDays)}");
-            logger.ZLogTrace($"siteNumber:{workDay.siteNumber},siteName:{workDay.siteName},workDayCount:{workDay.workDayCount},workDays:{convertDateTimeToDate(workDay.workDays)}");
+            logger.ZLogTrace($"siteKey:{workDay.siteKey},siteNumber:{workDay.siteNumber},siteName:{workDay.siteName},status:{workDay.status},workDayCount:{workDay.workDayCount},workDays:{convertDateTimeToDate(workDay.workDays)}");
         }
         logger.ZLogTrace($"== end print ==");
     }
@@ -287,6 +369,36 @@ logger.ZLogInformation($"[checkWorkDayAtDayOfWeek] {convertDateTimeToDate(dicHol
             {
                 sb.Append(" & ");
             }
+        }
+        return sb.ToString();
+    }
+
+    private string convertDateTimeToDateAndDayofweek(DateTime day)
+    {
+        StringBuilder sb = new StringBuilder();
+        switch (day.DayOfWeek)
+        {
+        case DayOfWeek.Sunday:
+            sb.Append(day.ToString("yyyy/MM/dd(日)"));
+            break;
+        case DayOfWeek.Monday:
+            sb.Append(day.ToString("yyyy/MM/dd(月)"));
+            break;
+        case DayOfWeek.Tuesday:
+            sb.Append(day.ToString("yyyy/MM/dd(火)"));
+            break;
+        case DayOfWeek.Wednesday:
+            sb.Append(day.ToString("yyyy/MM/dd(水)"));
+            break;
+        case DayOfWeek.Thursday:
+            sb.Append(day.ToString("yyyy/MM/dd(木)"));
+            break;
+        case DayOfWeek.Friday:
+            sb.Append(day.ToString("yyyy/MM/dd(金)"));
+            break;
+        case DayOfWeek.Saturday:
+            sb.Append(day.ToString("yyyy/MM/dd(土)"));
+            break;
         }
         return sb.ToString();
     }
@@ -313,20 +425,27 @@ logger.ZLogInformation($"[checkWorkDayAtDayOfWeek] {convertDateTimeToDate(dicHol
 public class MyConfig
 {
     public int FirstDataRow {get; set;} = -1;
+    public int SiteKeyColumn {get; set;} = -1;
     public int SiteNumberColumn {get; set;} = -1;
     public int SiteNameColumn {get; set;} = -1;
+    public int StatusColumn {get; set;} = -1;
     public int WorkDayCountColumn {get; set;} = -1;
     public int WorkDaysColumn {get; set;} = -1;
     public string FirstExcelSheetName {get; set;} = "";
     public string SecondExcelSheetName {get; set;} = "";
     public string PublicHolidaysInJapan {get; set;} = "";
     public string BussinessHolidays {get; set;} = "";
+    public string CheckStatusAtSurvey {get; set;} = "";
+    public string CheckStatusAtWork {get; set;} = "";
+    
 }
 
 public class MyWorkDay
 {
+    public string siteKey = "";
     public string siteNumber = "";
     public string siteName = "";
+    public string status = "";
     public int workDayCount = -1;
     public List<DateTime> workDays = new List<DateTime>();
 }
